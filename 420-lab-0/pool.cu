9bdd27be6d8aa9d45ceb0667baa865aeec217ae4
JulianArmour <35701671+JulianArmour@users.noreply.github.com>
2020-09-26 22:35:51 -0400

    added kernel execution time

--- 420-lab-0/pool.cu
+++ 420-lab-0/pool.cu
@@ -6,7 +6,8 @@
 
 __global__ void pool(unsigned char output_image[], const unsigned char image[], unsigned int width_out, unsigned int height_out) {
   // perspective: image is an array of pixel (x), with 4 channels (z) per pixel
-  // A thread is given to each output channel per pixel.
+  // A thread is given to each output channel in each pixel of the output image.
+
   int pixel_out = blockDim.x * blockIdx.x + threadIdx.x;
   int index_out = pixel_out * blockDim.z + threadIdx.z;  // index = pixel + channel
   int y_out = pixel_out / width_out;
@@ -43,10 +44,22 @@ int main(int argc, char** argv) {
   cudaMalloc((void**)&d_old_image, width * height * 4 * sizeof(unsigned char));
   cudaMemcpy((void*)d_old_image, (void*)old_image, width * height * 4 * sizeof(unsigned char), cudaMemcpyHostToDevice);
   cudaMallocManaged((void**)&new_image, width * height * sizeof(unsigned char)); // width/2 * height/2 * 4channels = width * height
+
+  //start timer
+  float memsettime;
+  cudaEvent_t start, stop;
+  cudaEventCreate(&start); cudaEventCreate(&stop);
+  cudaEventRecord(start, 0);
   
   pool<<<(width * height + (no_threads * 4) - 1) / (no_threads * 4), dim3(no_threads, 1, 4)>>>(new_image, d_old_image, width / 2, height / 2);
+  //cudaDeviceSynchronize();
 
-  cudaDeviceSynchronize();
+  //stop timer
+  cudaEventRecord(stop, 0);
+  cudaEventSynchronize(stop);
+  cudaEventElapsedTime(&memsettime, start, stop);
+  printf("Kernel execution time: %f\n", memsettime);
+  cudaEventDestroy(start); cudaEventDestroy(stop);
 
   lodepng_encode32_file(output_name, new_image, width / 2, height / 2);
 
