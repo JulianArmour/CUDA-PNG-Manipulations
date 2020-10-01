0a2b0a46049a160f64a91de0b86a530e44337b35
JulianArmour <35701671+JulianArmour@users.noreply.github.com>
2020-10-01 18:05:56 -0400

    added defensive argument check

--- 420-lab-0/pool.cu
+++ 420-lab-0/pool.cu
@@ -12,9 +12,9 @@ __global__ void pool(unsigned char output_image[], const unsigned char image[], 
   int index_out = pixel_out * blockDim.z + threadIdx.z;  // index = pixel + channel
   int y_out = pixel_out / width_out;
   int x_out = pixel_out - y_out * width_out;
-  // pixel index of the input image. Each output pixel is mapped to the the first 2x2 pixel block of the input image.
+  // pixel index of the input image. Each output pixel is mapped to the the first pixel of the 2x2 pixel block of the input image.
   int pixel_in = (y_out * width_out * 4 + x_out * 2) * 4;
-  //printf("Pixel_in = %d  y_out = %d  x_out = %d\n", pixel_in, y_out, x_out);
+
   if (index_out >= width_out * height_out * 4) return;
 
   // look at each pixel value in the 2x2 pixel block, choose the largest value.
@@ -34,6 +34,9 @@ int main(int argc, char** argv) {
   char* input_name = argv[1];
   char* output_name = argv[2];
   int no_threads = atoi(argv[3]);
+  int no_x_threads = no_threads / 4;
+  if (no_x_threads > 256) no_x_threads = 256;
+  if (no_x_threads == 0) no_x_threads = 1;
 
   unsigned char *old_image, *d_old_image, *new_image;
   unsigned int width, height;
@@ -51,8 +54,7 @@ int main(int argc, char** argv) {
   cudaEventCreate(&start); cudaEventCreate(&stop);
   cudaEventRecord(start, 0);
   
-  pool<<<(width * height + (no_threads * 4) - 1) / (no_threads * 4), dim3(no_threads, 1, 4)>>>(new_image, d_old_image, width / 2, height / 2);
-  //cudaDeviceSynchronize();
+  pool<<<(width * height + (no_x_threads * 4) - 1) / (no_x_threads * 4), dim3(no_x_threads, 1, 4)>>>(new_image, d_old_image, width / 2, height / 2);
 
   //stop timer
   cudaEventRecord(stop, 0);
